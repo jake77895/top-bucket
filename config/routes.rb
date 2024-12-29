e0ddee403265677ec12f524f50b8cfe6b5982712
jake77895 <jspitzer@chicagobooth.edu>
2024-12-29 18:07:36 +0000

    Created initial landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
   
+  # Navigation parts of the site
+  root "site#home"
+  get "/about", to: "site#about"
+  get "/contact", to: "site#contact"
+
+
 end
