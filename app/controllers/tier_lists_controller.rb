4198931e324834bbff7545b163454915f85f7ab0
jake77895 <jspitzer@chicagobooth.edu>
2025-01-04 00:49:23 +0000

    Updated tier list view

--- app/controllers/tier_lists_controller.rb
+++ app/controllers/tier_lists_controller.rb
@@ -1,5 +1,9 @@
 class TierListsController < ApplicationController
   def show
     @tier_list = TierList.find(params[:id])
+    @items = @tier_list.items.includes(:picture_attachment)
+    @item_custom_values = ItemRank.where(tier_list_id: @tier_list.id).includes(:item).map do |rank|
+      { item_id: rank.item_id, custom_values: rank.custom_values }
+    end
   end
 end
