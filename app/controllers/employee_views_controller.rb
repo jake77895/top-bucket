f295e8f1a5d1f4713a7d5a4200586439889e34d3
jake77895 <jspitzer@chicagobooth.edu>
2025-01-12 02:59:07 +0000

    Minor logic adjustments

--- app/controllers/employee_views_controller.rb
+++ app/controllers/employee_views_controller.rb
@@ -5,7 +5,7 @@ class EmployeeViewsController < ApplicationController
 
     # Filtered Ratings: Using params to dynamically scope
     @filtered_ratings = @employee.ratings
-                                .where(form_context: params[:form_context], position_type: params[:position_type])
+                                .where(form_context: params[:form_context].to_s.downcase, position_type: params[:position_type])
                                 .includes(:user)
 
     overview_summary # Call the overview_summary method to set variables
@@ -19,16 +19,16 @@ class EmployeeViewsController < ApplicationController
 
     # Scoped Ratings: Dynamically filter based on `form_context` and `position_type`
     @filtered_ratings = @employee.ratings
-                              .where(form_context: params[:form_context], employee: @employee)
+                              .where(form_context: params[:form_context].to_s.downcase, employee: @employee)
                               .includes(:user)
                               .order(created_at: :desc)
 
-    if params[:form_context] == 'networking' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'networking' && @employee.position_type_id == 2
       calculate_ib_networking_summary_metrics(@filtered_ratings)
       overall_ib_networking_impression(@filtered_ratings)
     end
 
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
       calculate_ib_interview_summary_metrics(@filtered_ratings)
       overall_ib_interview_impression(@filtered_ratings)
       recieved_job_offer_ib_interview(@filtered_ratings)
@@ -105,10 +105,11 @@ private
 
     # Dynamically find question IDs based on question texts
     if params[:form_context] == 'networking' && @employee.position_type_id == 2
-      technical_question_id = FormTemplate.find_by(question_text: "Were you asked any technical questions?")&.id.to_s
-      deal_question_id = FormTemplate.find_by(question_text: "Were you asked about a deal?")&.id.to_s
-      trend_question_id = FormTemplate.find_by(question_text: "Were you asked about a market trend?")&.id.to_s
-      referred_question_id = FormTemplate.find_by(question_text: "Were you referred to another colleague?")&.id.to_s
+
+      technical_question_id = FormTemplate.find_by(question_text: "Were you asked any technical questions?", form_context: params[:form_context])&.id.to_s
+      deal_question_id = FormTemplate.find_by(question_text: "Were you asked about a deal?", form_context: params[:form_context].to_s.downcase)&.id.to_s
+      trend_question_id = FormTemplate.find_by(question_text: "Were you asked about a market trend?", form_context: params[:form_context].to_s.downcase)&.id.to_s
+      referred_question_id = FormTemplate.find_by(question_text: "Were you referred to another colleague?", form_context: params[:form_context].to_s.downcase)&.id.to_s
 
       # Calculate percentages dynamically using the question IDs
       @asked_technical = (responses.count { |r| r[technical_question_id] == "yes" } * 100.0 / responses.size).round
@@ -118,8 +119,8 @@ private
     end
 
     # Most common tones
-    if params[:form_context] == 'networking' && @employee.position_type_id == 2
-      tone_question_id = FormTemplate.find_by(question_text: "How would you describe the tone of the conversation?")&.id.to_s
+    if params[:form_context].to_s.downcase == 'networking' && @employee.position_type_id == 2
+      tone_question_id = FormTemplate.find_by(question_text: "How would you describe the tone of the conversation?", form_context: params[:form_context].to_s.downcase)&.id.to_s
       tones = responses.map { |r| r[tone_question_id] }.compact
       tone_counts = tones.tally
       @most_common_tones = tone_counts.sort_by { |_tone, count| -count }.take(2).map do |tone, count|
@@ -134,10 +135,12 @@ private
   def calculate_ib_interview_summary_metrics(ratings)
     responses = ratings.pluck(:responses).map(&:compact)
 
+
     # Dynamically find question IDs based on question texts
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
-      deal_question_id = FormTemplate.find_by(question_text: "Were you asked about a deal?")&.id.to_s
-      trend_question_id = FormTemplate.find_by(question_text: "Were you asked about a market trend?")&.id.to_s
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
+
+      deal_question_id = FormTemplate.find_by(question_text: "Were you asked about a deal?", form_context: params[:form_context].to_s.downcase)&.id.to_s
+      trend_question_id = FormTemplate.find_by(question_text: "Were you asked about a market trend?", form_context: params[:form_context].to_s.downcase)&.id.to_s
 
       # Calculate percentages dynamically using the question IDs
       @asked_deal = (responses.count { |r| r[deal_question_id] == "yes" } * 100.0 / responses.size).round
@@ -145,8 +148,8 @@ private
     end
 
     # Most common tones
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
-      tone_question_id = FormTemplate.find_by(question_text: "How would you describe the tone of the interview?")&.id.to_s
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
+      tone_question_id = FormTemplate.find_by(question_text: "How would you describe the tone of the interview?", form_context: params[:form_context].to_s.downcase)&.id.to_s
       tones = responses.map { |r| r[tone_question_id] }.compact
       tone_counts = tones.tally
       @most_common_tones = tone_counts.sort_by { |_tone, count| -count }.take(2).map do |tone, count|
@@ -159,9 +162,9 @@ private
   end
 
   def overall_ib_networking_impression(ratings)
-    if params[:form_context] == 'networking' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'networking' && @employee.position_type_id == 2
       # Fetch the ID dynamically based on the question text
-      impression_id = FormTemplate.find_by(question_text: "How would you rate your interaction?").id.to_s
+      impression_id = FormTemplate.find_by(question_text: "How would you rate your interaction?", form_context: params[:form_context].to_s.downcase)&.id.to_s
     
       # Count occurrences of each response for the "How would you rate your interaction?" question
       impression_counts = ratings.pluck(:responses).map { |r| r[impression_id] }.compact.tally
@@ -177,9 +180,9 @@ private
   end
 
   def overall_ib_interview_impression(ratings)
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
       # Fetch the ID dynamically based on the question text
-      impression_id = FormTemplate.find_by(question_text: "What was your overall impression of the interview?").id.to_s
+      impression_id = FormTemplate.find_by(question_text: "What was your overall impression of the interview?", form_context: params[:form_context].to_s.downcase)&.id.to_s
     
       # Count occurrences of each response for the "What was your overall impression of the interview?" question
       impression_counts = ratings.pluck(:responses).map { |r| r[impression_id] }.compact.tally
@@ -194,9 +197,9 @@ private
   end
 
   def recieved_job_offer_ib_interview(ratings)
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
       # Fetch the ID dynamically based on the question text
-      offer_id = FormTemplate.find_by(question_text: "Did you receive a job offer?").id.to_s
+      offer_id = FormTemplate.find_by(question_text: "Did you receive a job offer?", form_context: params[:form_context].to_s.downcase)&.id.to_s
     
       # Count occurrences of each response for the "What was your overall impression of the interview?" question
       offer_counts = ratings.pluck(:responses).map { |r| r[offer_id] }.compact.tally
@@ -213,9 +216,9 @@ private
   end
 
   def technical_difficulty_ib_interview(ratings)
-    if params[:form_context] == 'interview' && @employee.position_type_id == 2
+    if params[:form_context].to_s.downcase == 'interview' && @employee.position_type_id == 2
       # Fetch the ID dynamically based on the question text
-      technical_difficulty_id = FormTemplate.find_by(question_text: "How difficult were the technical questions?").id.to_s
+      technical_difficulty_id = FormTemplate.find_by(question_text: "How difficult were the technical questions?", form_context: params[:form_context].to_s.downcase)&.id.to_s
     
       # Count occurrences of each response for the "What was your overall impression of the interview?" question
       technical_difficulty_counts = ratings.pluck(:responses).map { |r| r[technical_difficulty_id] }.compact.tally
