ff7596944bdf4aa73c8fe7af5717143db21b4154
jake77895 <jspitzer@chicagobooth.edu>
2025-01-14 00:53:21 +0000

    updated the star logic

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -2,15 +2,22 @@ class QuestionsUsersController < ApplicationController
   before_action :authenticate_user!
 
   def toggle_star
+
     @question = Question.find(params[:question_id])
     questions_user = QuestionsUser.find_or_initialize_by(question: @question, user: current_user)
 
     if questions_user.persisted?
       questions_user.destroy
-      render json: { status: "unstarred" }, status: :ok
+      # flash[:notice] = "Question unstarred successfully."
     else
-      questions_user.save
-      render json: { status: "starred" }, status: :ok
+      if questions_user.save
+        # flash[:notice] = "Question starred successfully."
+      else
+        flash[:alert] = "Unable to star the question. Please try again."
+      end
     end
+
+    # Redirect back to the referring page
+    redirect_back fallback_location: root_path
   end
 end
