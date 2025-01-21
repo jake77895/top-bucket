fc7c1a62af155ca3a510963745fdff534e8d5211
jake77895 <jspitzer@chicagobooth.edu>
2025-01-21 19:28:43 +0000

    Updated the top and bottom bucket part of the home page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -12,27 +12,20 @@ class SiteController < ApplicationController
 
   def mock_home
 
-    ratings
+    tb_ratings
+    bb_ratings
 
   end
 
   private
 
-  def ratings
+  def tb_ratings
     
-    # Define the mapping of long names to short names
-    name_mapping = {
-      "Investment Banking" => "IB",
-      "Venture Capital" => "VC",
-      "Private Equity" => "PE",
-      "Hedge Fund Strategies" => "Hedge Funds",
-      "Investment Banking Restructuring Groups" => "IB Restructuring"
-    }
     # Fetch random tier lists to display on the home view
     random_tier_list_ids = TierList.order("RANDOM()").limit(3).pluck(:id) # Example: 3 random tier lists
   
     # Count unique user rankings for each tier list
-    @user_ranking_counts = ItemRank
+    @user_ranking_counts_tb = ItemRank
       .where(tier_list_id: random_tier_list_ids)
       .group(:tier_list_id)
       .select("tier_list_id, COUNT(DISTINCT user_id) as user_count")
@@ -40,7 +33,7 @@ class SiteController < ApplicationController
       .to_h
   
     # Fetch group rankings for items with an average rank of 1 or 2 within these tier lists
-    @group_rankings = ItemRank
+    @group_rankings_tb = ItemRank
       .where(tier_list_id: random_tier_list_ids)
       .group(:item_id, :tier_list_id)
       .select("item_id, tier_list_id, AVG(rank) as average_rank")
@@ -49,6 +42,31 @@ class SiteController < ApplicationController
       .limit(6)
 
   end
+
+  def bb_ratings
+    
+    # Fetch random tier lists to display on the home view
+    random_tier_list_ids = TierList.order("RANDOM()").limit(3).pluck(:id) # Example: 3 random tier lists
+  
+    # Count unique user rankings for each tier list
+    @user_ranking_counts_bb = ItemRank
+      .where(tier_list_id: random_tier_list_ids)
+      .group(:tier_list_id)
+      .select("tier_list_id, COUNT(DISTINCT user_id) as user_count")
+      .map { |record| [record.tier_list_id, record.user_count] }
+      .to_h
+  
+    # Fetch group rankings for items with an average rank of 1 or 2 within these tier lists
+    @group_rankings_bb = ItemRank
+      .where(tier_list_id: random_tier_list_ids)
+      .group(:item_id, :tier_list_id)
+      .select("item_id, tier_list_id, AVG(rank) as average_rank")
+      .having("AVG(rank) >= 5")
+      .order("RANDOM()")
+      .limit(6)
+
+  end
+
 
   # Helper method to shorten tier list names
   def shorten_name(name)
