d0903032025cc2653cdab8adb298e629fa0d29c7
jake77895 <jspitzer@chicagobooth.edu>
2025-02-05 23:11:26 +0000

    Updated the gem files to deal with amazon s3

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Added gems
 
+# For aws s3
+gem 'aws-sdk-s3'
+
 # For apple calendar
 gem 'icalendar'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Allow POST authenticity on Codespaces in dev
   config.action_controller.forgery_protection_origin_check = false
+
+  # Allow storage in Amazons3
+  config.active_storage.service = :amazon
 end
