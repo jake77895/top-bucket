3b40ec408c1f9a89703d3ba823f706707ce9cc46
Jacob Spitzer <jspitzer@chicagobooth.edu>
2025-02-13 18:19:24 -0600

    deleted active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_08_160945) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_000042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
 
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
@@ -461,7 +451,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_08_160945) do
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "career_compensations", "career_jobs", on_delete: :cascade
   add_foreign_key "career_jobs", "users"
